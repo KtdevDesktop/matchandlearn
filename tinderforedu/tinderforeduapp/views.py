0f316d017afa4cabd031697ad9f1cf74aa9e8e7d
Nattakrit Jatupattaradit <s6101012610037@email.kmutnb.ac.th>
2020-01-15 22:34:13 +0700

    Added register function.
    Now user can signup, login and logout from wepage.
    
    Co-Authored-By: watcharawut30160 <watcharawut30160@users.noreply.github.com>

--- tinderforedu/tinderforeduapp/views.py
+++ tinderforedu/tinderforeduapp/views.py
@@ -1,7 +1,28 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+from .models import Userinfo, Subject
 
 # Create your views here.
-from .models import Userinfo, Subject
+@login_required
+def home(request):
+    return render(request, 'tinder/home.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home.html')
+    else:
+        form = UserCreationForm()
+    return render(request, 'tinder/signup.html', {'form': form})
 def your_subject_page(request,user_id):
     if request.POST.get('subject_good'):
         subject = Subject.objects.create(subject_name=request.POST['subject_good'])
@@ -14,3 +35,4 @@ def home_page(request):
     if not Userinfo.objects.filter(name="watcharawut").exists():
         Userinfo.objects.create(name="watcharawut")
     return render(request, 'tinder/home.html',{'User': Userinfo.objects.filter(name="watcharawut")})
+
