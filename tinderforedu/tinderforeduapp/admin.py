508dc3d6d3198e6181ba7518e92e63f460c3a704
Pakkapong <s6101012630119@email.kmutnb.ac.th>
2020-02-13 14:34:27 +0700

    Register Savechat model to django admin page.

--- tinderforedu/tinderforeduapp/admin.py
+++ tinderforedu/tinderforeduapp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Profile, Userinfo
+from chat.models import Savechat
 admin.site.register(Profile)
 admin.site.register(Userinfo)
+admin.site.register(Savechat)
 # Register your models here.
