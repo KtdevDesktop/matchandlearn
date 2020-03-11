9cd5aabe0810a96c0b8bb975bf4ab2d96f621863
Nattakrit Jatupattaradit <s6101012610037@email.kmutnb.ac.th>
2020-03-11 14:39:32 +0700

    Add css and style to login page

--- tinderforedu/settings.py
+++ tinderforedu/settings.py
@@ -133,5 +133,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 LOGIN_REDIRECT_URL = '/'
 ASGI_APPLICATION = 'tinderforedu.routing.application'
