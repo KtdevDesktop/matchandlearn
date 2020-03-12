6346a5a569067893356978d1f2ede07b246122ed
Nattakrit Jatupattaradit <s6101012610037@email.kmutnb.ac.th>
2020-03-12 16:48:34 +0700

    Add allowed host to settings file

--- tinderforedu/settings.py
+++ tinderforedu/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['matchandlearn.herokuapp.com','matchandlearn.social']
 
 # Application definition
 
