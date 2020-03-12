dfcae8515f2d599a787909baa8e7553bf775a1e8
Nattakrit Jatupattaradit <s6101012610037@email.kmutnb.ac.th>
2020-03-12 21:43:17 +0700

    Edit setting for channels in server.

--- Procfile
+++ Procfile
@@ -1,1 +1,3 @@
 web: gunicorn tinderforedu.wsgi --log-file -
+web2: daphne tinderforedu.asgi:channel_layer --port $PORT --bind 0.0.0.0 -v2
+worker: python manage.py runworker -v2
--- tinderforedu/settings.py
+++ tinderforedu/settings.py
@@ -87,20 +87,11 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'tinderforedu.wsgi.application'
-
+ASGI_APPLICATION = 'tinderforedu.routing.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-'''DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'TEST': {
-            'NAME': os.path.join(BASE_DIR, 'db_test.sqlite3'),
-        }
-    }
-}'''
 DATABASES = {}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
