86b19a15dd321548fc5da1e13931bd5b522393c9
Nattakrit Jatupattaradit <s6101012610037@email.kmutnb.ac.th>
2020-03-12 22:22:40 +0700

    Edit setting and Procfile to run on Heroku server

--- tinderforedu/asgi.py
+++ tinderforedu/asgi.py
@@ -1,12 +1,9 @@
-"""
-ASGI entrypoint. Configures Django and then runs the application
-defined in the ASGI_APPLICATION setting.
-"""
-
 import os
 import django
+from channels.layers import get_channel_layer
 from channels.routing import get_default_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tinderforedu.settings")
 django.setup()
+channel_layer = get_channel_layer()
 application = get_default_application()
