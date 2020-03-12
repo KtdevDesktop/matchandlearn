40f3ff58fb3a239cda5a8c9df301d6e60283800d
Nattakrit Jatupattaradit <s6101012610037@email.kmutnb.ac.th>
2020-03-12 23:29:45 +0700

    add CELERY_BROKER_URL in setting

--- tinderforedu/settings.py
+++ tinderforedu/settings.py
@@ -113,6 +113,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+CELERY_BROKER_URL = [os.environ.get('REDIS_URL', 'redis://localhost:6379')]
+CELERY_RESULT_BACKEND = [os.environ.get('REDIS_URL', 'redis://localhost:6379')]
+
 CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
