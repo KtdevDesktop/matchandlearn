b050911aa6f5444f4564ee732c29af826381724d
Nattakrit Jatupattaradit <42247051+cpre20068@users.noreply.github.com>
2020-03-26 03:58:15 +0700

    Add default picture for facebook user

--- tinderforeduapp/pipeline.py
+++ tinderforeduapp/pipeline.py
@@ -1,4 +1,4 @@
-from .models import Userinfo
+from .models import Userinfo, Profilepic
 from django.contrib.auth.models import User
 from social_core.pipeline.user import get_username as social_get_username
 from datetime import date, datetime
@@ -22,19 +22,20 @@ def user_profile_db(backend, user, response, *args, **kwargs):
         if response.get('gender') == 'female':
             gender = 'Female'
         if response.get('email') == None:
-            Userinfo.objects.create(name=(response.get('name')).split(" ")[0] + (response.get('name')).split(" ")[1],
+            user = Userinfo.objects.create(name=(response.get('name')).split(" ")[0] + (response.get('name')).split(" ")[1],
                                     school='',
                                     age=age,
                                     fullname=(response.get('name')).split(" ")[0],
                                     lastname=(response.get('name')).split(" ")[1],
                                     bio=gender, fb_link=response.get('link'))
         else:
-            Userinfo.objects.create(name=(response.get('email')).split("@")[0],
+            user = Userinfo.objects.create(name=(response.get('email')).split("@")[0],
                                 school='',
                                 age=age,
                                 fullname=(response.get('name')).split(" ")[0],
                                 lastname=(response.get('name')).split(" ")[1],
                                 bio=gender, fb_link=response.get('link'))
+        Profilepic.objects.create(user=user, images='default.png')
 
 def get_username(strategy, details, backend, user=None, *args, **kwargs):
     result = social_get_username(strategy, details, backend, user=user, *args, **kwargs)
